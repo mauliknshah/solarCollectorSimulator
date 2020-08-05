5c9e8b6e5a705a6d704883fd9e6edb9f5f2c26c2
mauliknshah <shahmaulik09@gmail.com>
2020-08-05 14:54:57 -0500

    Adds Collector class.

---
+++ io/simulation/collector/components/SolarCollector.java
@@ -0,0 +1,27 @@
+package io.simulation.collector.components;
+
+public class SolarCollector {
+    double areaSqM;
+    double efficiency;
+
+    SolarCollector(double areaSqM, double efficiency){
+        this.areaSqM = areaSqM;
+        this.efficiency = efficiency;
+    }
+
+    public double getAreaSqM() {
+        return areaSqM;
+    }
+
+    public void setAreaSqM(double areaSqM) {
+        this.areaSqM = areaSqM;
+    }
+
+    public double getEfficiency() {
+        return efficiency;
+    }
+
+    public void setEfficiency(double efficiency) {
+        this.efficiency = efficiency;
+    }
+}
