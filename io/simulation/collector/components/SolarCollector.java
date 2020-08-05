4de6a2f4487424fb5fdddde20c82c006830c37a9
mauliknshah <shahmaulik09@gmail.com>
2020-08-05 18:00:16 -0500

    Corrects the equation in collector heating.

--- io/simulation/collector/components/SolarCollector.java
+++ io/simulation/collector/components/SolarCollector.java
@@ -1,7 +1,7 @@
 package io.simulation.collector.components;
 
 public class SolarCollector {
-    static final double WATER_HEAT_CAPACITY = 4182;
+    static final double WATER_HEAT_CAPACITY = 4179.6;
     double areaSqM;
     double efficiency;
 
