81ea361a391df5fa1b18d4516fbd6f99fecd2e04
mauliknshah <shahmaulik09@gmail.com>
2020-08-05 18:02:29 -0500

    Updates WaterTank to initialze the temperature correctly.

--- io/simulation/collector/components/WaterTank.java
+++ io/simulation/collector/components/WaterTank.java
@@ -2,20 +2,19 @@ package io.simulation.collector.components;
 import java.lang.Math;
 
 public class WaterTank {
-    static final double WATER_HEAT_CAPACITY = 4182;
+    static final double WATER_HEAT_CAPACITY = 4179.6;
     double tankVolumeInL;
     double waterVolumeInL;
     double tankInHeightInM;
     double tankOutHeightInM;
     double tankTempK;
     double tankHeatEnergyInJ;
-    double tankHeatLossPerSecondInPercent;
-
+    double tankHeatLossPerSecondInFactor;
     double initialTempK;
 
     public WaterTank ( double tankVolumeInL, double waterVolumeInL, double tankInHeightInM,
                 double tankOutHeightInM, double tankTempK, double tankHeatEnergyInJ,
-                double tankHeatLossPerSecondInPercent){
+                double tankHeatLossPerSecondInFactor){
         
         if(tankInHeightInM < tankOutHeightInM){
             throw new AssertionError("Tank Input must be higher than the output height.");
@@ -26,16 +25,17 @@ public class WaterTank {
         this.tankInHeightInM = tankInHeightInM;
         this.tankOutHeightInM = tankOutHeightInM;
         this.tankTempK = tankTempK;
+        this.initialTempK = tankTempK;
         this.tankHeatEnergyInJ = tankHeatEnergyInJ;
-        this.tankHeatLossPerSecondInPercent = tankHeatLossPerSecondInPercent;
+        this.tankHeatLossPerSecondInFactor = tankHeatLossPerSecondInFactor;
     }
 
 
 
     public void tankHeatLossAfterStep(){
-        double heatLoss = Math.max(0.01 * this.tankHeatLossPerSecondInPercent * this.tankHeatEnergyInJ,0);
+        double heatLoss = Math.max(0.01 * this.tankHeatLossPerSecondInFactor * this.tankHeatEnergyInJ,0);
         this.tankHeatEnergyInJ -= heatLoss;
-        this.tankTempK += (-1*heatLoss)/ this.waterVolumeInL * this.WATER_HEAT_CAPACITY;
+        this.tankTempK += (-1*heatLoss)/ (this.waterVolumeInL * this.WATER_HEAT_CAPACITY);
     }
 
     public void tankHeatLossFromWaterOut(double waterVolumeLoss){
@@ -46,7 +46,7 @@ public class WaterTank {
     public void tankHeatGainFromWaterIn(double waterVolumeInL, double heatInJ){
         this.tankHeatEnergyInJ += heatInJ;
         this.waterVolumeInL += Math.min(waterVolumeInL, this.tankVolumeInL);
-        this.tankTempK += (heatInJ)/ this.waterVolumeInL * this.WATER_HEAT_CAPACITY;
+        this.tankTempK += (heatInJ)/ (this.waterVolumeInL * this.WATER_HEAT_CAPACITY);
     }
 
     public double getTankPumpingHeight(){
@@ -83,8 +83,8 @@ public class WaterTank {
         return tankHeatEnergyInJ;
     }
 
-    public double getTankHeatLossPerSecondInPercent() {
-        return tankHeatLossPerSecondInPercent;
+    public double getTankHeatLossPerSecondInFactor() {
+        return tankHeatLossPerSecondInFactor;
     }
 
     public double getInitialTempK() {
