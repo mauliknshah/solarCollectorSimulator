2a6fd54cbd999b40660ce00e96a085b91c652c94
mauliknshah <shahmaulik09@gmail.com>
2020-08-05 14:54:20 -0500

    Adds the SolarCOllector initial file.

---
+++ io/simulation/collector/SolarCollectorSimulation.java
@@ -0,0 +1,26 @@
+package io.simulation.collector;
+import io.simulation.*;
+
+
+public class SolarCollectorSimulation implements SimulationInterface{
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void step() {
+
+    }
+
+    @Override
+    public ObservationInterface observe() {
+        return null;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
